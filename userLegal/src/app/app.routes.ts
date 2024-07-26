04757f7f9b675a2a1a6f8ac057f88d6eebeecd0b
Ezequielp19 <85073571+Ezequielp19@users.noreply.github.com>
2024-07-26 00:46:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ userLegal/src/app/app.routes.ts
@@ -0,0 +1,55 @@
+import { Productoferta } from './common/models/productofree.model';
+import { Routes,RouterModule } from '@angular/router';
+
+import { HomeComponent } from './views/home/home.component';
+import { ProductosPage } from '../app/views/marcas/proucto/producto/producto.component';
+import { GestionEtiquetasPage } from '../app/views/marcas/etiquetas/etiquetas.component';
+import { CategoriasPage } from '../app/views/marcas/categoria/categoria/categoria.component';
+import { MarcasPage } from '../app/views/marcas/marcas.component';
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+   {
+    path: 'marcas',
+    loadComponent: () => import('../app/views/marcas/marcas.component').then((m) => m.MarcasPage),
+  },
+   {
+    path: 'usuarios',
+    loadComponent: () => import('../app/views/marcas/usuarios/usuarios.component').then((m) => m.UsuariosPage),
+  },
+
+     {
+    path: 'categoria',
+    loadComponent: () => import('../app/views/marcas/categoria/categoria/categoria.component').then((m) => m.CategoriasPage),
+  },
+
+    {
+    path: 'productos',
+    loadComponent: () => import('../app/views/marcas/proucto/producto/producto.component').then((m) => m.ProductosPage),
+  },
+   {
+    path: 'agregarProducto',
+    loadComponent: () => import('../app/views/marcas/agregar-producto/agregar-producto.component').then((m) => m.AgregarProductoPage),
+  },
+   {
+    path: 'agregar-categoria',
+    loadComponent: () => import('../app/views/marcas/agregar-categoria/agregar-categoria.component').then((m) => m.AgregarCategoriaComponent),
+  },
+  {
+    path: 'agregar-marca',
+    loadComponent: () => import('../app/views/marcas/agregar-marca/agregar-marca.component').then((m) => m.AgregarMarcaComponent),
+  },
+
+    {
+    path: 'productosOferta',
+    loadComponent: () => import('../app/views/marcas/oferta-product/oferta-product.component').then((m) => m.OfertaProductComponent),
+  },
+
+
+    {
+    path: 'etiquetas',
+    loadComponent: () => import('../app/views/marcas/etiquetas/etiquetas.component').then((m) => m.GestionEtiquetasPage),
+  },
+
+];
